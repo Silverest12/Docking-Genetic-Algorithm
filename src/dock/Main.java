be4d0bc6b7456d7675c5e9c41bc81dcd3a17f7f4
silverest <silverest12@gmail.com>
2020-12-16 15:59:48 +0100

    fixed generation system

--- src/dock/Main.java
+++ src/dock/Main.java
@@ -8,9 +8,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] arr) {
-        Scanner in = new Scanner(System.in);
-        String fileName = in.nextLine();
-        in.close();
+        //Scanner in = new Scanner(System.in);
+        //String fileName = in.nextLine();
+        //in.close();
+        String fileName = "inputTest.txt";
         DockData dock = TextParser.convertTextToDoc(FileUtils.readFile(fileName));
 
         Population pop = new Population(4, dock);
--- src/geneticAlgorithm/Population.java
+++ src/geneticAlgorithm/Population.java
@@ -14,7 +14,7 @@ public class Population {
     private final int elemLength;
     private final List<String> elemPool;
     private final List<String> postePool;
-    private ArrayList<DNA> popDNAs;
+    private final ArrayList<DNA> popDNAs;
     private final DockData dockData;
 
     public Population(int n, DockData dockData) {
@@ -37,38 +37,96 @@ public class Population {
             return dnaMap;
         }
 
-        private boolean checkIfOverlapped (ArrayList<Navire> listNvr, int startTime) {
+        public int calcFitness () {
+            int sum = 0;
+            for (String p : dnaMap.keySet()) {
+                for (Navire n : dnaMap.get(p)) {
+                    sum += n.getHeureDeb() - n.getHeureArr();
+                }
+            }
+            return sum;
+        }
+
+        private boolean checkIfOverlapped (ArrayList<Navire> listNvr, int startTime, int dureeServ) {
+            int finServ = startTime + dureeServ;
             if (listNvr == null) return true;
             else {
                 for (Navire n : listNvr) {
-                    if (n.getHeureDeb() <= startTime && startTime < n.getHeureDeb() + n.getDureeServ())
+                    if (n.getHeureDeb() <= startTime && startTime < n.getHeureDeb() + n.getDureeServ()
+                        && n.getHeureDeb() <= finServ && finServ < n.getHeureDeb() + n.getDureeServ())
                         return false;
                 }
             }
             return true;
         }
 
-        private HashMap<String, ArrayList<Navire>> generateDNA () {
+        private HashMap<String, ArrayList<Navire>> firstGen () {
             HashMap<String, ArrayList<Navire>> dnaMap = new HashMap<>();
             List<String> elemPoolCopy = new ArrayList<>(elemPool);
             Random rand = new Random();
-            int mapSize = 0;
-            while (mapSize != elemLength) {
-                String addr = elemPoolCopy.get(rand.nextInt(elemPoolCopy.size()));
-                String poste = postePool.get(rand.nextInt(postePool.size()));
-                int heureArr = dockData.getHeureArr(addr);
-                int dureeServ = dockData.getDureeServ(addr, poste);
-                int heureDeb = (int) (rand.nextDouble() * (1440 - dureeServ) + heureArr);
-                if (dureeServ != 0 && checkIfOverlapped(dnaMap.get(poste), heureDeb)) {
-                    Navire n = new Navire(addr, heureArr, heureDeb, dureeServ);
-                    dnaMap.putIfAbsent(poste, new ArrayList<>());
-                    dnaMap.get(poste).add(n);
-                    elemPoolCopy.remove(addr);
-                    mapSize ++;
-                }
+
+            for (String poste : postePool) {
+                String addr;
+                int heureArr;
+                int dureeServ;
+
+                do {
+                    addr = elemPoolCopy.get(rand.nextInt(elemPoolCopy.size()));
+                    heureArr = dockData.getHeureArr(addr);
+                    dureeServ = dockData.getDureeServ(addr, poste);
+                } while (dureeServ == 0);
+
+                Navire n = new Navire(addr, heureArr, heureArr, dureeServ);
+                dnaMap.put(poste, new ArrayList<>());
+                dnaMap.get(poste).add(n);
+                elemPoolCopy.remove(addr);
             }
 
             return dnaMap;
+        }
+
+        private HashMap<String, ArrayList<Navire>> generateDNA () {
+            HashMap<String, ArrayList<Navire>> dnaMap = firstGen();
+            List<String> elemPoolCopy = new ArrayList<>(elemPool);
+
+            for(String str : dnaMap.keySet()) {
+                for (Navire n : dnaMap.get(str)) {
+                    elemPoolCopy.remove(n.getIdNavire());
+                }
+            }
+
+            Random rand = new Random();
+            int mapSize = dnaMap.size();
+            while (mapSize != elemLength) {
+                String addr;
+                String poste;
+                int dureeServ;
+                int heureDeb;
+
+                do {
+                    addr = elemPoolCopy.get(rand.nextInt(elemPoolCopy.size()));
+                    poste = postePool.get(rand.nextInt(postePool.size()));
+                    dureeServ = dockData.getDureeServ(addr, poste);
+                } while (dureeServ == 0);
+
+                int heureArr = dockData.getHeureArr(addr);
+
+                do {
+                    heureDeb = rand.nextInt(1440 - dureeServ);
+                } while (heureDeb < heureArr || !checkIfOverlapped(dnaMap.get(poste), heureDeb, dureeServ));
+
+                Navire n = new Navire(addr, heureArr, heureDeb, dureeServ);
+                //dnaMap.putIfAbsent(poste, new ArrayList<>());
+                dnaMap.get(poste).add(n);
+                elemPoolCopy.remove(addr);
+                mapSize ++;
+            }
+
+            return dnaMap;
+        }
+
+        private String toHour(int time) {
+            return time / 60 +  "h" + (time % 60 == 0? "" : time % 60);
         }
 
         @Override
@@ -77,7 +135,7 @@ public class Population {
 
             strBld.append(String.format("%14s | ", " "));
             for (String poste: dnaMap.keySet()) {
-                int i = dnaMap.get(poste).size() * 5;
+                int i = dnaMap.get(poste).size() * 6;
                 strBld.append(String.format("%" + i + "s | ", poste));
             }
             strBld.append("\n");
@@ -85,24 +143,9 @@ public class Population {
             strBld.append(String.format("%14s | ", "idNavire"));
             for(String p: dnaMap.keySet()) {
                 strBld.append(" ");
-                if (dnaMap.get(p).size() == 1) {
-                    strBld.append(" ");
-                }
-                for (Navire nav: dnaMap.get(p)) {
-                    strBld.append(String.format("%4s ", nav.getIdNavire()));
-                }
-                strBld.append("| ");
-            }
-            strBld.append("\n");
 
-            strBld.append(String.format("%14s | ", "Heure Debut"));
-            for(String p: dnaMap.keySet()) {
-                strBld.append(" ");
-                if (dnaMap.get(p).size() == 1) {
-                    strBld.append(" ");
-                }
                 for (Navire nav: dnaMap.get(p)) {
-                    strBld.append(String.format("%4d ", nav.getHeureDeb()));
+                    strBld.append(String.format("%5s ", nav.getIdNavire()));
                 }
                 strBld.append("| ");
             }
@@ -112,11 +155,20 @@ public class Population {
 
             for(String p: dnaMap.keySet()) {
                 strBld.append(" ");
-                if (dnaMap.get(p).size() == 1) {
-                    strBld.append(" ");
-                }
+
                 for (Navire nav: dnaMap.get(p)) {
-                    strBld.append(String.format("%4d ", nav.getHeureArr()));
+                    strBld.append(String.format("%5s ", toHour(nav.getHeureArr())));
+                }
+                strBld.append("| ");
+            }
+            strBld.append("\n");
+
+            strBld.append(String.format("%14s | ", "Heure Debut"));
+            for(String p: dnaMap.keySet()) {
+                strBld.append(" ");
+
+                for (Navire nav: dnaMap.get(p)) {
+                    strBld.append(String.format("%5s ", toHour(nav.getHeureDeb())));
                 }
                 strBld.append("| ");
             }
@@ -125,16 +177,15 @@ public class Population {
             strBld.append(String.format("%14s | ", "Duree service"));
             for(String p: dnaMap.keySet()) {
                 strBld.append(" ");
-                if (dnaMap.get(p).size() == 1) {
-                    strBld.append(" ");
-                }
+
                 for (Navire nav: dnaMap.get(p)) {
-                    strBld.append(String.format("%4d ", nav.getDureeServ()));
+                    strBld.append(String.format("%5d ", nav.getDureeServ()));
                 }
                 strBld.append("| ");
             }
-            strBld.append("\n");
-
+            strBld.append("\n")
+                    .append("Fitness : ").append(calcFitness())
+                    .append("\n");
             return strBld.toString();
         }
     }
@@ -142,6 +193,10 @@ public class Population {
     public void generatePop () {
         for (int i = 0; i < popNumber; i++)
             popDNAs.add(new DNA());
+    }
+
+    public void printFit () {
+        popDNAs.stream().map(DNA::calcFitness).sorted().forEach(System.out::println);
     }
 
     public void print() {
